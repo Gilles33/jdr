9dcf7af4f396c2700f904cd831d3b1f26f643bce
Gilles <gilles.martinez33@gmail.com>
2018-02-15 23:06:10 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+jdr
+===
+
+A Symfony project created on February 15, 2018, 9:47 pm.
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,172 @@
+<?php
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $strength;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $defense;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $health;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set strength
+     *
+     * @param integer $strength
+     *
+     * @return User
+     */
+    public function setStrength($strength)
+    {
+        $this->strength = $strength;
+
+        return $this;
+    }
+
+    /**
+     * Get strength
+     *
+     * @return integer
+     */
+    public function getStrength()
+    {
+        return $this->strength;
+    }
+
+    /**
+     * Set defense
+     *
+     * @param integer $defense
+     *
+     * @return User
+     */
+    public function setDefense($defense)
+    {
+        $this->defense = $defense;
+
+        return $this;
+    }
+
+    /**
+     * Get defense
+     *
+     * @return integer
+     */
+    public function getDefense()
+    {
+        return $this->defense;
+    }
+
+    /**
+     * Set health
+     *
+     * @param integer $health
+     *
+     * @return User
+     */
+    public function setHealth($health)
+    {
+        $this->health = $health;
+
+        return $this;
+    }
+
+    /**
+     * Get health
+     *
+     * @return integer
+     */
+    public function getHealth()
+    {
+        return $this->health;
+    }
+}
